5d342b1a4d66181644f54f14c6077c7d85d2ba2e
biplavt <biplavt@hawsco.com>
2019-01-17 11:16:08 -0800

    Added index.js

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket=io();
+
+socket.on('connect',function(){
+	console.log('connected to server: client');
+
+	
+	socket.emit('createMessage',{
+		from:'client',
+		text:'yes yes'
+	})
+});
+
+	
+socket.on('disconnect',function(){
+	console.log('disconnect:client');
+});
+
+socket.on('newMessage',function(newMessage){
+	console.log('New message arrived:',newMessage);
+})
